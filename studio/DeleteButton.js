94a99ef3517688db55547ca94c0ad34e6bef4e46
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-02-04 13:45:21 +0100

    refactor to node 8 and new req structure

--- studio/DeleteButton.js
+++ studio/DeleteButton.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react'
 import ReportEditor from './ReportEditor'
 
 export default class DeleteButton extends Component {
-  static propTypes = {
-    tab: React.PropTypes.object,
-    onUpdate: React.PropTypes.func.isRequired
-  }
-
   render () {
     if (!this.props.tab || (this.props.tab.key !== 'Reports') || !ReportEditor.Instance || !ReportEditor.Instance.ActiveReport) {
       return <div />
@@ -16,4 +11,9 @@ export default class DeleteButton extends Component {
       <i className='fa fa-trash' />Delete
     </div>
   }
+}
+
+DeleteButton.propTypes = {
+  tab: React.PropTypes.object,
+  onUpdate: React.PropTypes.func.isRequired
 }
