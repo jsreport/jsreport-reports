6252db48a869e4d8eafe9d66ed78a640c18957c6
BJR Matos <bjrmatos@gmail.com>
2018-07-18 14:00:29 -0500

    fix not showing Donwload, Delete buttons of an open reports

--- studio/DeleteButton.js
+++ studio/DeleteButton.js
@@ -2,12 +2,16 @@ import React, { Component } from 'react'
 import ReportEditor from './ReportEditor'
 
 export default class DeleteButton extends Component {
+  getReportEditorInstance () {
+    return ReportEditor.default ? ReportEditor.default.Instance : ReportEditor.Instance
+  }
+
   render () {
-    if (!this.props.tab || (this.props.tab.key !== 'Reports') || !ReportEditor.Instance || !ReportEditor.Instance.ActiveReport) {
+    if (!this.props.tab || (this.props.tab.key !== 'Reports') || !this.getReportEditorInstance() || !this.getReportEditorInstance().ActiveReport) {
       return <div />
     }
 
-    return <div className='toolbar-button' onClick={() => ReportEditor.Instance.remove()}>
+    return <div className='toolbar-button' onClick={() => this.getReportEditorInstance().remove()}>
       <i className='fa fa-trash' />Delete
     </div>
   }
--- studio/DownloadButton.js
+++ studio/DownloadButton.js
@@ -3,14 +3,20 @@ import ReportEditor from './ReportEditor'
 import Studio from 'jsreport-studio'
 
 export default class DownloadButton extends Component {
+  getReportEditorInstance () {
+    return ReportEditor.default ? ReportEditor.default.Instance : ReportEditor.Instance
+  }
+
   download () {
-    if (ReportEditor.Instance && ReportEditor.Instance.ActiveReport) {
-      window.open(`${Studio.rootUrl}/reports/${ReportEditor.ActiveReport._id}/attachment`, '_self')
+    const instance = this.getReportEditorInstance()
+
+    if (instance && instance.ActiveReport) {
+      window.open(`${Studio.rootUrl}/reports/${instance.ActiveReport._id}/attachment`, '_self')
     }
   }
 
   render () {
-    if (!this.props.tab || (this.props.tab.key !== 'Reports') || !ReportEditor.Instance || !ReportEditor.Instance.ActiveReport) {
+    if (!this.props.tab || (this.props.tab.key !== 'Reports') || !this.getReportEditorInstance() || !this.getReportEditorInstance().ActiveReport) {
       return <div />
     }
 
